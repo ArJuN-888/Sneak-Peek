1ec1dfc5f389a5e1ceaf29d41cc462566f4c9f5c
ArJuN-888 <ksarjun635@gmail.com>
2024-01-11 19:42:44 +0530

    Edited app and deleted some file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState,useEffect } from 'react';
-import './App.css';
+import { useState} from 'react';
 import myContext from './Components/Context';
 import { proData } from './Components/Json';
 import Navbar from './Components/Navbar'
